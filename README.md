19b8c440e3654ab6aad8f3001304e70446ce1c6f
Sam VanTassel <vantassel.sam@gmail.com>
2022-01-24 17:45:11 -0800

    build: setup webpack and installed packages

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# some-app
+# Sharexp
+
+## Helps you with the job hunt
+It has a robust set of useful features
---
+++ webpack.config.js
@@ -0,0 +1,84 @@
+// const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const config = {
+  entry: [
+    'react-hot-loader/patch',
+    './src/index.tsx'
+  ],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.png$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              mimetype: 'image/png'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.svg$/,
+        use: 'file-loader'
+      },
+      {
+        test: /\.ts(x)?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      templateContent: ({ htmlWebpackPlugin }) => '<!DOCTYPE html><html><head><meta charset=\"utf-8\"><title>' + htmlWebpackPlugin.options.title + '</title></head><body><div id=\"app\"></div></body></html>',
+      filename: 'index.html',
+    }),
+    new MiniCssExtractPlugin()
+  ],
+  devServer: {
+    'static': {
+      directory: './dist'
+    }
+  },
+  resolve: {
+    extensions: [
+      '.tsx',
+      '.ts',
+      '.jsx',
+      '.js'
+    ],
+    alias: {
+      'react-dom': '@hot-loader/react-dom'
+    }
+  }
+};
+
+module.exports = config;
