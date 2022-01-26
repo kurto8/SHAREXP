9155597eb30e6607fa402164a4664783299916b1
Sam VanTassel <vantassel.sam@gmail.com>
2022-01-25 17:12:10 -0800

    feat: middleware to add ids to requests

--- server/controllers/companies.js
+++ server/controllers/companies.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const companyController = {};
 
-// middlerware for get requests to companies endpoint
+// gets all companies
 companyController.getCompanies = (req, res, next) => {
   const sql = `SELECT * FROM "companies";
   `;
@@ -17,6 +17,7 @@ companyController.getCompanies = (req, res, next) => {
     .catch((err) => next(err));
 };
 
+// adds logo and formatted company name to res.locals
 companyController.addLogo = (req, res, next) => {
   const options = {
     headers: {
@@ -28,13 +29,13 @@ companyController.addLogo = (req, res, next) => {
     .then(response => response.json())
     .then((response) => {
       res.locals.logo = response[0] ? response[0].logo : null;
-      res.locals.name = response[0] ? response[0].name :req.body.name;
+      res.locals.name = response[0] ? response[0].name : req.body.name;
       return next();
     })
     .catch((err) => next(err))
-}
+};
 
-// middlerware for post requests to companies endpoint
+// creates a new company
 companyController.addCompany = (req, res, next) => {
   const sql = 
   `INSERT INTO "companies" (name, logo)
@@ -43,7 +44,7 @@ companyController.addCompany = (req, res, next) => {
   `
   const params = [ res.locals.name, res.locals.logo ];
   db.query(sql, params)
-  .then((data) => {
+    .then((data) => {
       console.log(data.rows);
       res.locals.id = data.rows;
       return next();
@@ -51,4 +52,40 @@ companyController.addCompany = (req, res, next) => {
     .catch((err) => next(err));
 };
 
+// creates a new entry
+companyController.createEntry = (req, res, next) => {
+  const sql =
+  `INSERT INTO posts COLUMNS (company_id, title, interview_questions, user_id, salary_range, red_flags, make_anonymous, pros, cons, notes, location_id, position_id, level_id)
+  `
+  const params = [
+    req.params.companyId, 
+    req.body.title,
+    req.body.interviewQuestions || null,
+    req.body.userID,
+    req.body.salaryRange || null,
+    req.body.redFlags || null,
+    req.body.makeAnonymous || false,
+    req.body.pros || null,
+    req.body.cons || null,
+    req.body.notes || null,
+    res.locals.locationId,
+    res.locals.position, 
+    res.locals.level,
+  ];
+
+  // what do we do with tags?? req.body.tags || null,
+  // db.query(sql, params)
+  // .then((data) => {
+  //   res.locals.postId = data.rows;
+  //   return next();
+  //   })
+  //   .catch((err) => next(err));
+  return next();
+};
+
 export default companyController;
+
+/* <-- ~~**~~**~~**~~** SCRATCHPAD **~~**~~**~~**~~**~~ -->
+
+
+*/ 
---
+++ server/controllers/global.js
@@ -0,0 +1,82 @@
+import db from "../models/dataModel.js";
+
+const globalController = {};
+
+// takes a loction name on request body and adds locationID to res.locals
+globalController.getLocationId = (req, res, next) => {
+  const sql = `SELECT id FROM locations WHERE name=$1
+    `;
+  const params = [req.body.location];
+  db.query(sql, params)
+    .then((data) => {
+      if (!data.rows[0]) {
+        const sql2 = `
+              INSERT INTO locations (name) VALUES ($1) RETURNING id`;
+        db.query(sql2, params)
+          .then((data) => {
+            res.locals.locationId = data.rows[0].id;
+            return next();
+          })
+          .catch((err) => next(err));
+      } else {
+        res.locals.locationId = data.rows[0].id;
+        return next();
+      }
+    })
+    .catch((err) => next(err));
+};
+
+// takes a position name on request body and adds positionID to res.locals
+globalController.getPositionId = (req, res, next) => {
+    const sql = `SELECT id FROM positions WHERE name=$1
+      `;
+    const params = [req.body.position];
+    db.query(sql, params)
+      .then((data) => {
+        if (!data.rows[0]) {
+          const sql2 = `
+                INSERT INTO positions (name) VALUES ($1) RETURNING id`;
+          db.query(sql2, params)
+            .then((data) => {
+              res.locals.positionId = data.rows[0].id;
+              return next();
+            })
+            .catch((err) => next(err));
+        } else {
+          res.locals.positionId = data.rows[0].id;
+          return next();
+        }
+      })
+      .catch((err) => next(err));
+};
+
+  // takes a level "name" on request body and adds levelID to res.locals
+  globalController.getLevelId = (req, res, next) => {
+    const sql = `SELECT id FROM levels WHERE name=$1
+      `;
+    const params = [req.body.level];
+    db.query(sql, params)
+      .then((data) => {
+        if (!data.rows[0]) {
+          const sql2 = `
+                INSERT INTO levels (name) VALUES ($1) RETURNING id`;
+          db.query(sql2, params)
+            .then((data) => {
+              res.locals.levelId = data.rows[0].id;
+              return next();
+            })
+            .catch((err) => next(err));
+        } else {
+          res.locals.levelId = data.rows[0].id;
+          return next();
+        }
+      })
+      .catch((err) => next(err));
+};
+
+export default globalController;
+
+/* <-- ~~**~~**~~**~~** SCRATCHPAD **~~**~~**~~**~~**~~ -->
+
+
+*/ 
